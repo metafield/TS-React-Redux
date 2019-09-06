88495d2378fb0fb3fa8fbb1588131371c9776acc
Anthony Sylvester <Anthony.Sylvester99@gmail.com>
2019-09-06 11:38:19 -0700

    reducer for delete todo

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -4,6 +4,8 @@ export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.FetchTodos:
       return action.payload;
+    case ActionTypes.DeleteTodo:
+      return state.filter((todo: Todo) => todo.id !== action.payload);
     default:
       return state;
   }
