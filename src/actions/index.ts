58798fd96649f606e6b7d2b6f44d59075c4ec887
Anthony Sylvester <Anthony.Sylvester99@gmail.com>
2019-09-06 10:48:36 -0700

    adds types to action, actiontype enum

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,14 +1,21 @@
 import axios from "axios";
 import { Dispatch } from "redux";
+import { ActionTypes } from "./Types";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 const url = `https://jsonplaceholder.typicode.com/todos`;
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const reponse = await axios.get(url);
+    const reponse = await axios.get<Todo[]>(url);
 
     dispatch({
-      type: "FETCH_TODOS",
+      type: ActionTypes.FetchTodos,
       payload: reponse.data
     });
   };
